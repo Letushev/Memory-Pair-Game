1b8bc4a0be11adcd5c7a0d31e7a87259aa4e27fd
Olexiy <o.m.letushev@gmail.com>
2018-04-03 08:21:16 +0300

    game is listening for user clicks

--- app2.js
+++ app2.js
@@ -15,9 +15,11 @@ function shuffleArray(array) {
 
 class Game {
   constructor(container, cardTypes, levelParams) {
-    this.level = 1;
-
     this.container = container;
+
+    this.level = 1;
+    this.hearts = 1;
+
     this.cardTypes = cardTypes;
     this.levelParams = levelParams;
   }
@@ -26,7 +28,7 @@ class Game {
     this.clicksRemain = this.levelParams.clicks;
 
     this.container.innerHTML = '';
-    this.showCurrentState();
+    this.fillGameState();
     this.fillGameArea();
 
     this.container.addEventListener('click', function(event) {
@@ -34,12 +36,12 @@ class Game {
     }.bind(this));
   }
 
-  showCurrentState() {
+  fillGameState() {
     const header = document.createElement('header');
     header.innerHTML = `
-      <span class="level">Level ${ this.level }</span>
+      <p>Level <span class="level">${ this.level }</span></p>
       <img class="logo-img" src="images/logo.png">
-      <span class="clicks">Clicks: ${ this.clicksRemain }</span> `;
+      <p>Clicks: <span class="clicks">${ this.clicksRemain }</span></p> `;
 
     this.container.appendChild(header);
   }
@@ -65,9 +67,14 @@ class Game {
     return document.querySelectorAll('.opened');
   }
 
+  get closedCards() {
+    return document.querySelectorAll('.closed');
+  }
+
   open(card) {
-    console.log('here');
     card.classList.add('opened');
+
+    this.hasClicked();
   }
 
   close(cards) {
@@ -75,7 +82,9 @@ class Game {
       cards.forEach(function(card) {
         card.classList.add('closed');
       });
-    }, 300);
+
+      this.checkVictory();
+    }.bind(this), 300);
   }
 
   hide(cards) {
@@ -119,6 +128,35 @@ class Game {
   compareOpenCards() {
     if (this.openedCards[0].className === this.openedCards[1].className) {
       this.close(this.openedCards);
+    }
+  }
+
+  hasClicked() {
+    this.clicksRemain--;
+
+    if (this.clicksRemain === 0) {
+      return this.restart();
+    }
+
+    this.updateCurrentState();
+  }
+
+  updateCurrentState() {
+    document.querySelector('.clicks').textContent = this.clicksRemain;
+    document.querySelector('.level').textContent = this.level;
+  }
+
+  restart() {
+    if (this.hearts === 0) {
+
+    } else {
+      this.start();
+    }
+  }
+
+  checkVictory() {
+    if (this.closedCards.length === this.levelParams.numberOfCards) {
+      console.log('victory');
     }
   }
 }
